d7e9d745eabd3788926a7e36d1ea887bc9f6f789
yemulambika <ambikayemul2001@gmail.com>
2026-04-28 21:14:54 +0530

    fix CORS preflight for signup/login

--- Backend1/server.cjs
+++ Backend1/server.cjs
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;;
 // Middleware
 
 app.use(express.urlencoded({ extended: true }));
-const cors = require("cors");
+
 
 app.use(cors({
   origin: "https://weather-dashboard-frontend-1.onrender.com",
